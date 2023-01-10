6f8704ba3668240ecaaaab61cd0ae9394238a0d1
shibinSk <shibinskombanal@gmail.com>
2023-01-10 10:34:24 +0530

    Netflix

---
+++ src/components/NavBar/navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function navbar() {
+  return (
+    <div className='navbar'>
+        <img src="" alt="Not Found" />
+      
+    </div>
+  )
+}
+
+export default navbar
