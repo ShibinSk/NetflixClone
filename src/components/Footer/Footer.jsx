40aba1f9fe2b5ae43d6fba5aabbfaaa82cf82a9a
shibinSk <shibinskombanal@gmail.com>
2023-01-11 18:13:18 +0530

    finish

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,11 +1,44 @@
-import React from 'react'
-import './Footer.css'
+import React from "react";
+import "./Footer.css";
 function Footer() {
   return (
-    <div className='footer'>
-    <h1 >FOOTER</h1>
+    <div className="footer">
+      <div>
+        
+        <img className='footer-logo' src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/1920px-Netflix_2015_logo.svg.png" alt="Not Found" />
+      </div>
+      <div className="Footer-content">
+        <ul>
+          <li>About</li>
+          <li>Movies</li>
+          <li>Comady</li>
+          <li>Horror</li>
+          <li>Romance</li>
+        </ul>
+        <ul>
+          <li>About</li>
+          <li>Movies</li>
+          <li>Comady</li>
+          <li>Horror</li>
+          <li>Romance</li>
+        </ul>
+        <ul>
+          <li>About</li>
+          <li>Movies</li>
+          <li>Comady</li>
+          <li>Horror</li>
+          <li>Romance</li>
+        </ul>
+        <ul>
+          <li>About</li>
+          <li>Movies</li>
+          <li>Comady</li>
+          <li>Horror</li>
+          <li>Romance</li>
+        </ul>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Footer
+export default Footer;
